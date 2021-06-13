e5d88042a5cebcbde58677e86296615bb273a3ce
Beto Frega <robertofrega@gmail.com>
2021-06-13 14:01:07 -0300

    chore init

---
+++ packages/dominio/src/lib/dominio.spec.ts
@@ -0,0 +1,7 @@
+import { dominio } from './dominio';
+
+describe('dominio', () => {
+  it('should work', () => {
+    expect(dominio()).toEqual('dominio');
+  });
+});
---
+++ packages/dominio/src/lib/dominio.ts
@@ -0,0 +1,3 @@
+export function dominio(): string {
+  return 'dominio';
+}
